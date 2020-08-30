ad6b29903e9ccba3177f82b590881074f5195f5c
subnit <subnit@163.com>
2020-08-30 16:30:13 +0800

    dev gitUtil

--- src/main/java/com/subnit/base/data/FileUtil.java
+++ src/main/java/com/subnit/base/data/FileUtil.java
@@ -1,6 +1,13 @@
 package com.subnit.base.data;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * description:
@@ -27,5 +34,89 @@ public class FileUtil {
         }
         return dir.delete();
     }
+
+    public static List<String> readTxtByLine(String filePath) {
+        List<String> res = new ArrayList<>();
+        FileInputStream fis = null;
+        FileChannel inChannel = null;
+        int bufSize = 1024*10;
+        try {
+            fis = new FileInputStream(filePath);
+            inChannel = fis.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(bufSize);
+            String enterStr = "\n";
+            StringBuilder strBuf = new StringBuilder("");
+            while(inChannel.read(buffer) != -1){
+                int rSize = buffer.position();
+                buffer.clear();
+                String tempString = new String(buffer.array(), 0, rSize);
+                if(fis.available() ==0){
+                    tempString+="\n";
+                }
+
+                int fromIndex = 0;
+
+                int endIndex;
+                while ((endIndex = tempString.indexOf(enterStr, fromIndex)) != -1) {
+                    String line = tempString.substring(fromIndex, endIndex);
+                    line = strBuf.toString() + line;
+                    strBuf.delete(0, strBuf.length());
+                    fromIndex = endIndex + 1;
+                    res.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(inChannel != null){
+                try{
+                    inChannel.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     *  get value from properties file
+     * @param key key
+     * @param path properties file payh
+     * @return value
+     */
+    public static String getSourcingValueBykey(String key, String path) {
+        String value = "";
+        try {
+            FileInputStream inputFile = new FileInputStream(path);
+            Properties properties = new Properties();
+            properties.load(inputFile);
+            inputFile.close();
+            value = properties.getProperty(key);
+            if (DataUtil.isEmpty(value)) {
+                return value;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+        String filePath = "src/main/resources/token.properties";
+        System.out.println(filePath);
+        System.out.println(getSourcingValueBykey("username",filePath));
+    }
+
+
 
 }
